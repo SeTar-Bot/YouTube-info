2d683e5d5d8f3a48ca39a335919aa40eabce6d35
EhsanFox <EhsanFox@users.noreply.github.com>
2021-03-24 01:42:59 -0700

    💫 | A Little Cleaner and Better

--- index.js
+++ index.js
@@ -1,36 +1,32 @@
 // =============== Bot ===============
 const Discord  = require('discord.js');
-const fetch = require('node-fetch');
+const info = require('./info');
 const client = new Discord.Client();
 const token = "BOT_TOKEN";
 const ytid = "CHANNEL_ID";
 const ytkey = "YouTube_API_KEY";
-let url = `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${ytid}&key=${ytkey}`
 let settings = { method: "Get" };
+let updateTime = 3600000; // Update Youtube Information?
+let information;
 // ==============================
+setInterval(() => {
+  information = info(ytid, ytkey);
+}, updateTime);
 
-fetch(url, settings)
-  .then(res => res.json())
-  .then((json) => {
     client.on('ready', () => {
       console.log(`Logged in as ${client.user.tag}!`);
       const activities = [
-        { name: `${json.items[0].statistics.videoCount} Videos`, type: 'WATCHING' },
-        { name: `${json.items[0].statistics.subscriberCount} Subscribers`, type: 'WATCHING'},
-        { name: `${json.items[0].statistics.viewCount} Views`, type: 'WATCHING' },
+        { name: `${information.TotalVideos} Videos`, type: 'WATCHING' },
+        { name: `${information.Subscribers} Subscribers`, type: 'WATCHING'},
+        { name: `${information.TotalViews} Views`, type: 'WATCHING' },
       ];
-      client.user.setPresence({ status: 'idle', activity: activities[0] });  
-      let activity = 1;    
+      let activity = 0;  
       setInterval(() => {
-        activities[2] = { name: `${json.items[0].statistics.videoCount} Videos`, type: 'WATCHING' },
-        activities[3] = {name: `${json.items[0].statistics.subscriberCount} Subscribers`, type: 'WATCHING'},
-        activities[4] = { name: `${json.items[0].statistics.viewCount} Views`, type: 'WATCHING' },
         if (activity > 4) activity = 0;
         client.user.setActivity(activities[activity]);
         activity++;
       }, 5000);
     });
-  });
 
 // =============== End ===============
 client.login(token);
